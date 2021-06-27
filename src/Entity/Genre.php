f5ad445e0f4176ca089131c8e1b95632be2cef3a
Mason Miller <masonmiller11@gmail.com>
2021-06-27 14:02:04 -0400

    Add getter to Genre entity

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -1,9 +1,11 @@
 <?php
 	namespace App\Entity;
 
+	use Doctrine\Common\Collections\ArrayCollection;
 	use Doctrine\Common\Collections\Selectable;
 	use Doctrine\Common\Collections\Collection;
 	use Doctrine\ORM\Mapping as ORM;
+	use JetBrains\PhpStorm\Pure;
 
 	/**
 	 * @ORM\Entity()
@@ -20,12 +22,24 @@
 		 */
 		private ?string $name;
 
-
 		/**
 		 * @var Collection|Game[]|Selectable
 		 *
 		 * @ORM\OneToMany(targetEntity="App\Entity\Game", mappedBy="genre")
 		 */
 		private Collection|Selectable|array $games;
+
+		#[Pure] public function __construct() {
+
+			$this->games = new ArrayCollection();
+
+		}
+
+		/**
+		 * @return Collection|Game[]|Selectable
+		 */
+		public function getGames(): Collection|Selectable|array {
+			return $this->games;
+		}
 
 	}
