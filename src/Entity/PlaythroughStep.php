3fea84d8dde59c841e74a8896e4fff43d48b4b8f
Mason Miller <masonmiller11@gmail.com>
2021-06-27 18:16:58 -0400

    Create PlaythroughStepTrait

---
+++ src/Entity/PlaythroughStep.php
@@ -0,0 +1,78 @@
+<?php
+	namespace App\Entity;
+
+	use Doctrine\ORM\Mapping as ORM;
+
+	/**
+	 * @ORM\Entity()
+	 * @ORM\Table(name="playthrough_steps")
+	 */
+	class PlaythroughStep implements PlaythroughStepInterface {
+
+		use EntityTrait;
+
+		use PlaythroughStepTrait;
+
+		/**
+		 * @var User
+		 *
+		 * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="playthroughSteps")
+		 * @ORM\JoinColumn(nullable=false)
+		 */
+		private User $owner;
+
+		/**
+		 * @var Playthrough
+		 *
+		 * @ORM\ManyToOne(targetEntity="App\Entity\Playthrough", inversedBy="steps")
+		 * @ORM\JoinColumn(nullable=false)
+		 */
+		private Playthrough $playthrough;
+
+		/**
+		 * @var bool
+		 *
+		 * @ORM\Column(type="boolean")
+		 */
+		private bool $completed;
+
+		public function __construct(string $name, string $description, Playthrough $playthrough, User $owner) {
+
+			$this->name = $name;
+			$this->description = $description;
+			$this->playthrough = $playthrough;
+			$this->owner = $owner;
+
+		}
+
+		/**
+		 * @param bool $completed
+		 * @return static
+		 */
+		public function setCompleted(bool $completed): static {
+			$this->completed = $completed;
+			return $this;
+		}
+
+		/**
+		 * @return User
+		 */
+		public function getOwner(): User {
+			return $this->owner;
+		}
+
+		/**
+		 * @return Playthrough
+		 */
+		public function getPlaythrough(): Playthrough {
+			return $this->playthrough;
+		}
+
+		/**
+		 * @return bool
+		 */
+		public function isCompleted(): bool {
+			return $this->completed;
+		}
+
+	}
--- src/Entity/PlaythroughTemplateStep.php
+++ src/Entity/PlaythroughTemplateStep.php
@@ -11,19 +11,7 @@
 
 		use EntityTrait;
 
-		/**
-		 * @var string|null
-		 *
-		 * @ORM\Column(type="string", length=64)
-		 */
-		private ?string $name;
-
-		/**
-		 * @var string|null
-		 *
-		 * @ORM\Column(type="text")
-		 */
-		private ?string $description;
+		use PlaythroughStepTrait;
 
 		/**
 		 * @var PlaythroughTemplate
@@ -48,38 +36,6 @@
 			$this->playthroughTemplate = $playthroughTemplate;
 			$this->owner = $owner;
 
-		}
-
-		/**
-		 * @param string|null $name
-		 * @return static
-		 */
-		public function setName(?string $name): static {
-			$this->name = $name;
-			return $this;
-		}
-
-		/**
-		 * @param string|null $description
-		 * @return static
-		 */
-		public function setDescription(?string $description): static {
-			$this->description = $description;
-			return $this;
-		}
-
-		/**
-		 * @return string|null
-		 */
-		public function getName(): ?string {
-			return $this->name;
-		}
-
-		/**
-		 * @return string|null
-		 */
-		public function getDescription(): ?string {
-			return $this->description;
 		}
 
 		/**
