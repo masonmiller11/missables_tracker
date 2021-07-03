174688be8017076ac936cc2fbf0057fd763db369
Mason Miller <masonmiller11@gmail.com>
2021-07-02 20:47:38 -0400

    Fix GameController Create route

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,22 +2,18 @@
 
 	namespace App\Controller;
 
-	use App\DTO\Transformer\GameResponseDTOTransformer;
+	use App\DTO\Transformer\ResponseTransformer\GameResponseDTOTransformer;
 	use App\DTO\Transformer\RequestTransformer\GameRequestDTOTransformer;
 	use App\Entity\Game;
-	use App\Entity\PlaythroughTemplate;
 	use App\Repository\GameRepository;
-	use Doctrine\ORM\EntityManager;
 	use Doctrine\ORM\EntityManagerInterface;
 	use Doctrine\ORM\ORMException;
-	use JetBrains\PhpStorm\ArrayShape;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Component\Serializer\SerializerInterface;
-	use Symfony\Component\Validator\Constraints\Json;
 	use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 	class GameController extends AbstractController {
--- src/DTO/Transformer/RequestTransformer/GameRequestDTOTransformer.php
+++ src/DTO/Transformer/RequestTransformer/GameRequestDTOTransformer.php
@@ -2,11 +2,7 @@
 
 	namespace App\DTO\Transformer\RequestTransformer;
 
-	use App\DTO\Exception\UnexpectedTypeException;
 	use App\DTO\Request\GameRequestDTO;
-	use App\DTO\Response\GameResponseDTO;
-	use App\Entity\Game;
-	use App\Entity\PlaythroughTemplate;
 	use Symfony\Component\HttpFoundation\Request;
 
 	class GameRequestDTOTransformer {
@@ -17,7 +13,7 @@
 
 			$dto = new GameRequestDTO();
 
-			$dto->title = $data['request'];
+			$dto->title = $data['title'];
 			$dto->developer = $data['developer'];
 			$dto->genre = $data['genre'];
 			$dto->releaseDate = \DateTimeImmutable::createFromFormat('Y-m-d',$data['release_date']);
