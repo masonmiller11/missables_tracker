8563e09ec882f12ec18b876f97d75fa0f4e0fbae
Mason Miller <masonmiller11@gmail.com>
2021-07-05 16:17:17 -0400

    Finish list method on PlaythroughController

--- src/Controller/PlaythroughController.php
+++ src/Controller/PlaythroughController.php
@@ -2,34 +2,39 @@
 
 	namespace App\Controller;
 
-	use App\Entity\EntityInterface;
+	use App\Entity\Game;
+	use App\Entity\Playthrough;
 	use App\Entity\PlaythroughTemplate;
 	use App\Entity\User;
 	use Doctrine\Common\Collections\Collection;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Component\Serializer\SerializerInterface;
 
 	class PlaythroughController extends AbstractController {
 
 		/**
-		 * @Route(path="/games/{id<\d+>}", methods={"GET"}, name="games.read")
+		 * @Route(path="/playthroughs/{page<\d+>?1}", methods={"GET"}, name="playthroughs.read")
 		 *
-		 * @param string|int          $id
+		 * @param string|int          $page
 		 * @param SerializerInterface $serializer
 		 *
 		 * @return Response
 		 */
-		public function read(string|int $id, SerializerInterface $serializer): Response {
+		public function list(string|int $page, SerializerInterface $serializer): Response {
 
 			$user = $this->getUser();
 
 			assert($user instanceof User);
 
-			$playthroughs = $user->getPlaythroughs()->toArray();
+			$playthroughs = $user->getPlaythroughs();
 
-			return new Response($serializer->serialize($this->normalizeMany($playthroughs), 'json'), Response::HTTP_OK, [
+			return new Response($serializer->serialize($this->normalizeMany($playthroughs), 'json',[
+				'circular_reference_handler' => function ($object) {
+					return $object->getId();
+				}
+			]), Response::HTTP_OK, [
 				'Content-Type' => 'application/json'
 			]) ;
 
@@ -38,11 +43,15 @@
 		protected function normalizeMany(Collection $entities) :array {
 			return [
 				$entities->map(
-					fn (PlaythroughTemplate $playthroughTemplate) => [
-						'id'=>$playthroughTemplate->getId(),
-						'visibility'=>$playthroughTemplate->isVisible(),
-						'owner'=>$playthroughTemplate->getOwner(),
-						'votes'=>$playthroughTemplate->getVotes(),
+					fn (Playthrough $playthrough) => [
+						'id'=>$playthrough->getId(),
+						'visibility'=>$playthrough->isVisible(),
+						'owner'=>$playthrough->getOwner()->getUsername(),
+						'game'=> [
+							'id'=>$playthrough->getGame()->getId(),
+							'title'=>$playthrough->getGame()->getTitle()
+						],
+						'template'=>$playthrough->getTemplate()->getId()
 					]
 				)->toArray()
 			];
