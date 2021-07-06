256242430c30a1786504adcc403ce2abb306ff58
Mason Miller <masonmiller11@gmail.com>
2021-07-06 00:53:23 -0400

    Add WIP PlaythroughtTemplateController

---
+++ src/DTO/Transformer/ResponseTransformer/PlaythroughTemplateResponseDTOTransformer.php
@@ -0,0 +1,35 @@
+<?php
+	namespace App\DTO\Transformer\ResponseTransformer;
+
+	use App\DTO\Exception\UnexpectedTypeException;
+	use App\DTO\Response\PlaythroughResponseDTO;
+	use App\Entity\Playthrough;
+
+	class PlaythroughTemplateResponseDTOTransformer extends AbstractResponseDTOTransformer {
+
+		/**
+		 * @param $object
+		 *
+		 * @return PlaythroughResponseDTO|null
+		 */
+		public function transformFromObject($object) :?PlaythroughResponseDTO{
+
+			if (!$object instanceof Playthrough) {
+				throw new UnexpectedTypeException('Expected type of Playthrough but got' . \get_class($object));
+			}
+
+			$dto = new PlaythroughResponseDTO();
+			$dto->id = $object->getId();
+			$dto->visibility = $object->isVisible();
+			$dto->owner = $object->getOwner()->getUsername();
+			$dto->templateId = $object->getTemplate()->getId();
+			$dto->game = [
+				'id' => strval($object->getGame()->getId()),
+				'title' => $object->getGame()->getTitle()
+			];
+
+			return $dto;
+
+		}
+
+	}
