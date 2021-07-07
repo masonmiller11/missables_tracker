9412821ae9f5f69b96dc18bd42fab61fe74e3663
Mason Miller <masonmiller11@gmail.com>
2021-07-06 22:04:14 -0400

    Create PlaythroughTemplateController

--- src/Controller/PlaythroughTemplateController.php
+++ src/Controller/PlaythroughTemplateController.php
@@ -1,14 +1,15 @@
 <?php
 	namespace App\Controller;
 
-	use App\DTO\Transformer\ResponseTransformer\PlaythroughResponseDTOTransformer;
 	use App\DTO\Transformer\ResponseTransformer\PlaythroughTemplateResponseDTOTransformer;
 	use App\Entity\User;
+	use App\Repository\PlaythroughTemplateRepository;
+	use App\Utility\Responder;
+	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Serializer\SerializerInterface;
-	use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 	/**
 	 * Class PlaythroughTemplateController
@@ -18,59 +19,64 @@
 	 */
 	class PlaythroughTemplateController extends AbstractController {
 
-		private ValidatorInterface $validator;
+
 		private PlaythroughTemplateResponseDTOTransformer $templateResponseDTOTransformer;
+		private PlaythroughTemplateRepository $playthroughTemplateRepository;
 
-		public function __construct (ValidatorInterface $validator,
-									 playthroughTemplateResponseDTOTransformer $playthroughTemplateResponseDTOTransformer) {
+		public function __construct (playthroughTemplateResponseDTOTransformer $playthroughTemplateResponseDTOTransformer,
+									 PlaythroughTemplateRepository $playthroughTemplateRepository) {
 
-			$this->validator = $validator;
 			$this->templateResponseDTOTransformer = $playthroughTemplateResponseDTOTransformer;
+			$this->playthroughTemplateRepository = $playthroughTemplateRepository;
 
 		}
 
 		/**
-		 * @Route(path="/{page<\d+>?1}", methods={"GET"}, name="list")
+		 * @Route(path="/{page<\d+>?1}", methods={"GET"}, name="list_this_users")
 		 *
 		 * @param string|int          $page
-		 * @param SerializerInterface $serializer
 		 *
 		 * @return Response
 		 */
-		public function list(string|int $page, SerializerInterface $serializer): Response {
-			//TODO list either my templates or all public templates by author or game.
-			//TODO, we can use this to get rid of the private before transforming them!
-			// foreach($array as $elementKey => $element) {
-			// 	foreach($element as $valueKey => $value) {
-			// 		if($valueKey == 'id' && $value == 'searched_value'){
-			// 			//delete this particular object from the $array
-			// 			unset($array[$elementKey]);
-			// 		}
-			// 	}
-			// }
-			//TODO UGHHHHHHHHHHHHHH actually what we should do here is create a query builder. get from where game id
-			// equals whatever but visibility is 0. THEN transform and validate it as normal.
+		public function listThisUsers(string|int $page): Response {
 
 			$user = $this->getUser();
 			assert($user instanceof User);
 
 			$templates = $user->getPlaythroughTemplates();
 
-			$dto = $this->templateResponseDTOTransformer->transformFromObjects($templates);
+			return Responder::createResponse($templates, $this->templateResponseDTOTransformer);
 
-			$errors = $this->validator->validate($dto);
-			if (count($errors) > 0) {
-				$errorString = (string)$errors;
-				return new Response($errorString);
-			}
+		}
 
-			return new Response($serializer->serialize($dto, 'json',[
-				'circular_reference_handler' => function ($object) {
-					return $object->getId();
-				}
-			]), Response::HTTP_OK, [
-				'Content-Type' => 'application/json'
-			]) ;
+		/**
+		 * @Route(path="/bygame/{gameID<\d+>}", methods={"GET"}, name="list_by_game")
+		 *
+		 * @param int $gameID
+		 *
+		 * @return Response
+		 */
+		public function listByGame (int $gameID): Response {
+
+			$templates = $this->playthroughTemplateRepository->findByGame($gameID);
+
+			return Responder::createResponse($templates, $this->templateResponseDTOTransformer);
+
+		}
+
+		/**
+		 * @Route(path="/byauthor/{authorID<\d+>}", methods={"GET"}, name="list_by_author")
+		 *
+		 * @param int $authorID
+		 * @param SerializerInterface $serializer
+		 *
+		 * @return Response
+		 */
+		public function listByAuthor (int $authorID): Response {
+
+			$templates = $this->playthroughTemplateRepository->findByAuthor($authorID);
+
+			return Responder::createResponse($templates, $this->templateResponseDTOTransformer);
 
 		}
 
