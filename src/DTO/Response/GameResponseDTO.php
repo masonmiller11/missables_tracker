9c58bf85257c36fe79d3240b5a87c69692870d37
Mason Miller <masonmiller11@gmail.com>
2021-07-10 12:44:50 -0400

    Update GameResponseDTO & Transformer

--- src/DTO/Response/GameResponseDTO.php
+++ src/DTO/Response/GameResponseDTO.php
@@ -13,6 +13,56 @@
 
 		/**
 		 * @Assert\NotNull
+		 * @Assert\Type("integer")
+		 */
+		public mixed $id;
+
+		/**
+		 * @Assert\NotNull
+		 * @Assert\Type("float")
+		 */
+		public mixed $rating;
+
+		/**
+		 * @Assert\NotBlank()
+		 */
+		public mixed $summary;
+
+		/**
+		 * @Assert\NotBlank()
+		 */
+		public mixed $storyline;
+
+		/**
+		 * @Assert\NotBlank()
+		 */
+		public mixed $slug;
+
+		/**
+		 * @Assert\NotNull
+		 * @Assert\Type("array")
+		 */
+		public mixed $screenshots;
+
+		/**
+		 * @Assert\NotNull
+		 * @Assert\Type("array")
+		 */
+		public mixed $platforms;
+
+		/**
+		 * @Assert\NotBlank()
+		 */
+		public mixed $cover;
+
+		/**
+		 * @Assert\NotNull
+		 * @Assert\Type("array")
+		 */
+		public mixed $artworks;
+
+		/**
+		 * @Assert\NotNull
 		 * @Assert\DateTime(format="Y-m-d")
 		 */
 		public mixed $releaseDate;
