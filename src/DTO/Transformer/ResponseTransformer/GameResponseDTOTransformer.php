79fa8a0ffc0f828829375f0fb9605255917bd2b7
Mason Miller <masonmiller11@gmail.com>
2021-07-10 12:44:50 -0400

    Update GameResponseDTO & Transformer

--- src/DTO/Transformer/ResponseTransformer/GameResponseDTOTransformer.php
+++ src/DTO/Transformer/ResponseTransformer/GameResponseDTOTransformer.php
@@ -2,19 +2,32 @@
 	namespace App\DTO\Transformer\ResponseTransformer;
 
 	use App\DTO\Response\GameResponseDTO;
+	use App\Entity\EntityInterface;
+	use App\Entity\Game;
 	use App\Entity\PlaythroughTemplate;
 
 	class GameResponseDTOTransformer extends AbstractResponseDTOTransformer {
 
 		/**
-		 * @param $object
+		 * @param Game $object
 		 * @return GameResponseDTO
 		 */
 		public function transformFromObject($object): GameResponseDTO {
 
+			Assert($object instanceof Game);
+
 			$dto = new GameResponseDTO();
 			$dto->genre = $object->getGenre();
 			$dto->title = $object->getTitle();
+			$dto->id = $object->getId();
+			$dto->summary = $object->getSummary();
+			$dto->rating = $object->getRating();
+			$dto->storyline = $object->getStoryline();
+			$dto->slug = $object->getSlug();
+			$dto->screenshots = $object->getScreenshots();
+			$dto->platforms = $object->getPlatforms();
+			$dto->cover = $object->getCover();
+			$dto->artworks = $object->getArtworks();
 			$dto->releaseDate = $object->getReleaseDate()->format('Y-m-d');
 			$dto->playthroughTemplates = $object->getTemplates()->map(
 				fn(PlaythroughTemplate $playthroughTemplate) => [
