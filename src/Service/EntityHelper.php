86147f5474ea8d1842e24099ded3a3a6f9c67dec
Mason Miller <masonmiller11@gmail.com>
2021-07-10 15:03:42 -0400

    Add EntityHelper service

---
+++ src/Service/EntityHelper.php
@@ -0,0 +1,52 @@
+<?php
+	namespace App\Service;
+
+	use App\DTO\Response\GameDTO;
+	use App\Entity\Game;
+	use Doctrine\ORM\EntityManagerInterface;
+	use Symfony\Component\HttpFoundation\JsonResponse;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+	class EntityHelper {
+
+		private ValidatorInterface $validator;
+
+		private EntityManagerInterface $entityManager;
+
+		public function __construct(ValidatorInterface $validator,
+									EntityManagerInterface $entityManager) {
+
+			$this->validator = $validator;
+			$this->entityManager = $entityManager;
+
+		}
+
+		public function createGame (GameDTO $dto): Response {
+
+			$errors = $this->validator->validate($dto);
+
+			if (count($errors) > 0) {
+				$errorString = (string)$errors;
+				return new Response($errorString);
+			}
+
+			$releaseDateTimeImmutable = new \DateTimeImmutable(date('Y/m/d H:i:s', $dto->releaseDate));
+
+
+			$game = new Game(
+				$dto->genre, $dto->title, $dto->internetGameDatabaseID, $dto->screenshots, $dto->artworks, $dto->cover,
+				$dto->platforms, $dto->slug, $dto->rating, $dto->summary, $dto->storyline, $releaseDateTimeImmutable
+			);
+
+			$this->entityManager->persist($game);
+			$this->entityManager->flush();
+
+			return new JsonResponse([
+				'status' => 'game created'
+			],
+				Response::HTTP_CREATED);
+
+		}
+
+	}
