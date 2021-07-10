56720d5f36e869b349f7977476191c673b49f507
Mason Miller <masonmiller11@gmail.com>
2021-07-10 12:39:46 -0400

    Generate migration & .idea files

---
+++ migrations/Version20210709133251.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210709133251 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE games ADD slug VARCHAR(64) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_FF232B31FDCD9D5B ON games (internet_game_database_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_FF232B31FDCD9D5B ON games');
+        $this->addSql('ALTER TABLE games DROP slug');
+    }
+}
