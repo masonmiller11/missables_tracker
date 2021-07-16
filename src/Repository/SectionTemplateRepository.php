0f2a7c89960444e47203faf21ada4c0635e2ac61
Mason Miller <masonmiller11@gmail.com>
2021-07-16 12:12:47 -0400

    Create Playthrough and Section repositories

--- src/Repository/SectionTemplateRepository.php
+++ src/Repository/SectionTemplateRepository.php
@@ -18,30 +18,4 @@
 			parent::__construct($registry, SectionTemplate::class);
 		}
 
-		public function findByGame(int $gameId) {
-			$qb = $this->createQueryBuilder('template')
-				->andWhere('game.id = :gameId')
-				->andWhere('template.visibility = true')
-				->leftJoin('template.game', 'game')
-				->setParameter('gameId', $gameId)
-				->addOrderBy('template.votes', 'DESC');
-
-			$query = $qb->getQuery();
-
-			return $query->execute();
-		}
-
-		public function findByAuthor(int $authorId) {
-			$qb = $this->createQueryBuilder('template')
-				->andWhere('author.id = :authorId')
-				->andWhere('template.visibility = true')
-				->leftJoin('template.owner', 'author')
-				->setParameter('authorId', $authorId)
-				->addOrderBy('template.votes', 'DESC');
-
-			$query = $qb->getQuery();
-
-			return $query->execute();
-		}
-
 	}
