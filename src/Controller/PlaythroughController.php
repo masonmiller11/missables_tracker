bc7628511809574293e5bd52d4cbde6cfe3c285b
Mason Miller <masonmiller11@gmail.com>
2021-07-18 16:20:21 -0400

    Playthrough controllers extend AbstractBaseApiController

--- src/Controller/PlaythroughController.php
+++ src/Controller/PlaythroughController.php
@@ -2,53 +2,41 @@
 	namespace App\Controller;
 
 	use App\DTO\Transformer\ResponseTransformer\PlaythroughResponseDTOTransformer;
-	use App\Entity\User;
-	use App\Service\ResponseHelper;
-	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\Routing\Annotation\Route;
-	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
-	use Symfony\Component\Serializer\SerializerInterface;
 	/**
 	 * Class PlaythroughController
 	 *
 	 * @package App\Controller
 	 * @Route(path="/playthroughs", name="playthroughs.")
 	 */
-	class PlaythroughController extends AbstractController {
-
-		private PlaythroughResponseDTOTransformer $playthroughResponseDTOTransformer;
-
-		/**
-		 * @var ResponseHelper
-		 */
-		private ResponseHelper $responseHelper;
-
-		public function __construct (PlaythroughResponseDTOTransformer $playthroughResponseDTOTransformer,
-		                             ResponseHelper $responseHelper) {
-			$this->playthroughResponseDTOTransformer = $playthroughResponseDTOTransformer;
-			$this->responseHelper = $responseHelper;
-		}
+	final class PlaythroughController extends AbstractBaseApiController {
 
 		/**
 		 * @Route(path="/{page<\d+>?1}", methods={"GET"}, name="list")
 		 *
-		 * @param string|int          $page
-		 * @param SerializerInterface $serializer
-		 *
+		 * @param string|int $page
+		 * @param PlaythroughResponseDTOTransformer $transformer
 		 * @return Response
+		 * @throws \Exception
 		 */
-		public function list(string|int $page, SerializerInterface $serializer): Response {
+		public function list(string|int $page, PlaythroughResponseDTOTransformer $transformer): Response {
 
 			$user = $this->getUser();
 
-			assert($user instanceof User);
+			try {
 
-			$playthroughs = $user->getPlaythroughs();
+				$playthroughs = $user->getPlaythroughs();
 
-			$dtos = $this->playthroughResponseDTOTransformer->transformFromObjects($playthroughs);
+				$dtos = $this->transformMany($playthroughs, $transformer);
 
-			return $this->responseHelper->createResponseForMany($dtos);
+				return $this->responseHelper->createResponseForMany($dtos);
+
+			} catch (\Exception $e) {
+
+				return $this->responseHelper->createErrorResponse($e);
+
+			}
 
 		}
 
