6e558ab4b47993419034ae810578cd8893a11e27
Mason Miller <masonmiller11@gmail.com>
2021-07-20 18:52:48 -0400

    Create AbstractGameDTO

--- src/DTO/Game/GameDTO.php
+++ src/DTO/Game/GameDTO.php
@@ -1,15 +1,10 @@
 <?php
-	namespace App\DTO;
+	namespace App\DTO\Game;
 
-	use App\DTO\DTOInterface;
 	use Symfony\Component\Validator\Constraints as Assert;
+	use App\DTO\DTOInterface;
 
-	class GameDTO implements DTOInterface {
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $title;
+	final class GameDTO extends AbstractGameDTO implements DTOInterface {
 
 		/**
 		 * @Assert\Type("integer")
@@ -18,64 +13,10 @@
 
 		/**
 		 * @Assert\NotNull
-		 * @Assert\Type("integer")
-		 */
-		public mixed $internetGameDatabaseID;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("float")
-		 */
-		public mixed $rating;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $summary;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $storyline;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $slug;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $screenshots;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $platforms;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $cover;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $artworks;
-
-		/**
-		 * @Assert\NotNull
 		 * @Assert\DateTime(format="Y-m-d")
 		 */
 		public mixed $releaseDate;
 
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $genre;
 
 		/**
 		 * @Assert\NotNull
--- src/DTO/Game/IGDBGameResponseDTO.php
+++ src/DTO/Game/IGDBGameResponseDTO.php
@@ -1,75 +1,15 @@
 <?php
 
-	namespace App\DTO;
+	namespace App\DTO\Game;
 
-	use App\DTO\DTOInterface;
 	use Symfony\Component\Validator\Constraints as Assert;
+	use App\DTO\DTOInterface;
 
-	class IGDBGameResponseDTO implements DTOInterface {
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $title;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("integer")
-		 */
-		public mixed $internetGameDatabaseID;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("float")
-		 */
-		public mixed $rating;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $summary;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $storyline;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $slug;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $screenshots;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $platforms;
+	final class IGDBGameResponseDTO extends AbstractGameDTO implements DTOInterface {
 
 		/**
 		 * @Assert\NotBlank()
 		 */
 		public mixed $releaseDate;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $cover;
-
-		/**
-		 * @Assert\NotNull
-		 * @Assert\Type("array")
-		 */
-		public mixed $artworks;
-
-		/**
-		 * @Assert\NotBlank()
-		 */
-		public mixed $genre;
 
 	}
