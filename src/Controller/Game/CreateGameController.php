8a20e5b19f530f8bfd582003feb590b5d92e4da7
Mason Miller <masonmiller11@gmail.com>
2021-07-22 22:51:50 -0400

    Break out GameController into separate controllers

---
+++ src/Controller/Game/CreateGameController.php
@@ -0,0 +1,42 @@
+<?php
+	namespace App\Controller\Game;
+
+	use App\Controller\AbstractBaseApiController;
+	use App\DTO\Game\GameDTO;
+	use App\DTO\Transformer\RequestTransformer\GameRequestDTOTransformer;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+
+	/**
+	 * Class GameController
+	 *
+	 * @package App\Controller
+	 * @Route(path="/games/create", name="games.")
+	 */
+	final class CreateGameController extends AbstractBaseApiController {
+
+		/**
+		 * @Route(methods={"POST"}, name="create")
+		 *
+		 * @param Request $request
+		 * @param GameRequestDTOTransformer $transformer
+		 * @return Response
+		 * @throws \Exception
+		 */
+		public function create(Request $request, GameRequestDTOTransformer $transformer): Response {
+
+			$dto = $this->transformOne($request, $transformer);
+
+			Assert($dto instanceof GameDTO);
+			$this->validate($dto);
+
+			$game = $this->entityAssembler->assembleGame($dto);
+			$this->entityManager->persist($game);
+			$this->entityManager->flush();
+
+			return $this->responseHelper->returnResourceCreatedResponse('games/read/' . $game->getId());
+
+		}
+
+	}
