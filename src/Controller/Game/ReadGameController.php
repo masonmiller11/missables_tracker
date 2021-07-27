8253f3ea4d45c93c85216ae16a3c5116e7e5d3bc
Mason Miller <masonmiller11@gmail.com>
2021-07-26 23:12:04 -0400

    Clean up controller

--- src/Controller/Game/ReadGameController.php
+++ src/Controller/Game/ReadGameController.php
@@ -5,12 +5,11 @@
 	use App\Repository\GameRepository;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
-	use Symfony\Component\Serializer\SerializerInterface;
 
 	/**
 	 * Class GameController
 	 *
-	 * @package App\Controller
+	 * @package App\Controller\Game
 	 * @Route(path="/games/read", name="games.")
 	 */
 	final class ReadGameController extends AbstractBaseApiController {
@@ -18,16 +17,14 @@
 		/**
 		 * @Route(path="/{id<\d+>}", methods={"GET"}, name="read")
 		 *
-		 * @param string|int $id
+		 * @param string|int     $id
 		 * @param GameRepository $gameRepository
-		 * @param SerializerInterface $serializer
 		 *
 		 * @return Response
 		 *
 		 * Reads a single game from our database based on its id.
 		 */
-		public function read(string|int $id, GameRepository $gameRepository,
-							 SerializerInterface $serializer): Response {
+		public function read(string|int $id, GameRepository $gameRepository): Response {
 
 			$game = $gameRepository->find($id);
 
