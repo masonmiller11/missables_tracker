48aab7f861cbe9229f451393924503d6d1f431a1
Mason Miller <masonmiller11@gmail.com>
2021-07-26 23:12:16 -0400

    Create ReadPlaythroughTemplateController

--- src/Controller/PlaythroughTemplate/ReadPlaythroughTemplateController.php
+++ src/Controller/PlaythroughTemplate/ReadPlaythroughTemplateController.php
@@ -2,48 +2,32 @@
 	namespace App\Controller\PlaythroughTemplate;
 
 	use App\Controller\AbstractBaseApiController;
-	use App\DTO\Playthrough\PlaythroughTemplateDTO;
-	use App\DTO\Transformer\RequestTransformer\GameRequestDTOTransformer;
-	use App\DTO\Transformer\RequestTransformer\PlaythroughTemplateRequestDTOTransformer;
-	use App\Repository\GameRepository;
-	use App\Transformer\PlaythroughTemplateEntityTransformer;
-	use Symfony\Component\HttpFoundation\Request;
+	use App\Repository\PlaythroughTemplateRepository;
 	use Symfony\Component\HttpFoundation\Response;
-	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 	use Symfony\Component\Routing\Annotation\Route;
 
 	/**
-	 * Class CreatePlaythroughTemplateController
+	 * Class ReadPlaythroughTemplateController
 	 *
 	 * @package App\Controller\PlaythroughTemplate
-	 * @Route(path="/templates", name="templates.")
+	 * @Route(path="/templates/read", name="templates.")
 	 */
 	final class ReadPlaythroughTemplateController extends AbstractBaseApiController {
 
 		/**
-		 * @Route(path="/{id<\d+>}",methods={"read"}, name="read")
+		 * @Route(path="/{id<\d+>}",methods={"GET"}, name="read")
 		 *
-		 * @param Request                                  $request
-		 * @param PlaythroughTemplateRequestDTOTransformer $transformer
-		 * @param GameRepository                           $gameRepository
-		 * @param PlaythroughTemplateEntityTransformer     $playthroughTemplateEntityTransformer
+		 * @param string                        $id
+		 * @param PlaythroughTemplateRepository $playthroughTemplateRepository
 		 *
 		 * @return Response
 		 * @throws \Exception
 		 */
-		public function create(Request $request,
-							   PlaythroughTemplateRequestDTOTransformer $transformer,
-							   GameRepository $gameRepository,
-							   PlaythroughTemplateEntityTransformer $playthroughTemplateEntityTransformer): Response {
+		public function read(string $id, PlaythroughTemplateRepository $playthroughTemplateRepository): Response {
 
-			$playthroughTemplate = $this->doCreate($request,
-												   $transformer,
-											  PlaythroughTemplateDTO::class,
-												   $playthroughTemplateEntityTransformer
-												   );
+			$playthroughTemplate = $playthroughTemplateRepository->find($id);
 
-
-			return $this->responseHelper->returnResourceCreatedResponse('templates/read/' . $playthroughTemplate->getId());
+			return $this->responseHelper->createReadResponse($playthroughTemplate);
 
 		}
 	}
