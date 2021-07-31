c132633ab601a1b72ed1e5040b954a6a599f1eca
Mason Miller <masonmiller11@gmail.com>
2021-07-30 23:37:01 -0400

    Create playthrough

---
+++ src/Controller/Playthrough/CreatePlaythroughController.php
@@ -0,0 +1,47 @@
+<?php
+	namespace App\Controller\Playthrough;
+
+	use App\Controller\AbstractBaseApiController;
+	use App\DTO\Playthrough\PlaythroughDTO;
+	use App\DTO\Playthrough\PlaythroughTemplateDTO;
+	use App\DTO\Transformer\RequestTransformer\Playthrough\PlaythroughRequestDTOTransformer;
+	use App\DTO\Transformer\RequestTransformer\Playthrough\PlaythroughTemplateRequestDTOTransformer;
+	use App\Repository\GameRepository;
+	use App\Transformer\PlaythroughEntityTransformer;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+
+	/**
+	 * Class ListPlaythroughController
+	 *
+	 * @package App\Controller
+	 * @Route(path="/playthroughs/create", name="playthroughs.")
+	 */
+	class CreatePlaythroughController extends AbstractBaseApiController {
+
+		/**
+		 * @Route(methods={"POST"}, name="create")
+		 *
+		 * @param Request $request
+		 * @param PlaythroughRequestDTOTransformer $transformer
+		 * @param PlaythroughEntityTransformer $playthroughTemplateEntityTransformer
+		 *
+		 * @return Response
+		 * @throws \Exception
+		 */
+		public function create(Request $request,
+		                       PlaythroughRequestDTOTransformer $transformer,
+		                       PlaythroughEntityTransformer $playthroughTemplateEntityTransformer): Response {
+
+			$playthrough = $this->doCreate($request,
+				$transformer,
+				PlaythroughDTO::class,
+				$playthroughTemplateEntityTransformer
+			);
+
+			return $this->responseHelper->createResourceCreatedResponse('playthroughs/read/' . $playthrough->getId());
+
+		}
+
+	}
