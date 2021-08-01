0edaccf78ec73fe7a6c33335725f2afe647ca4c0
Mason Miller <masonmiller11@gmail.com>
2021-08-01 00:20:55 -0400

    Create controller

---
+++ src/Controller/Section/CreateSectionController.php
@@ -0,0 +1,48 @@
+<?php
+	namespace App\Controller\Section;
+
+	use App\Controller\AbstractBaseApiController;
+	use App\DTO\Playthrough\PlaythroughDTO;
+	use App\DTO\Playthrough\PlaythroughTemplateDTO;
+	use App\DTO\Section\SectionDTO;
+	use App\DTO\Transformer\RequestTransformer\Playthrough\PlaythroughRequestDTOTransformer;
+	use App\DTO\Transformer\RequestTransformer\Playthrough\PlaythroughTemplateRequestDTOTransformer;
+	use App\DTO\Transformer\RequestTransformer\Section\SectionRequestTransformer;
+	use App\Repository\GameRepository;
+	use App\Transformer\PlaythroughEntityTransformer;
+	use App\Transformer\SectionEntityTransformer;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+
+	/**
+	 * @package App\Controller
+	 * @Route(path="/section/create", name="section.")
+	 */
+	final class CreateSectionController extends AbstractBaseApiController {
+
+		/**
+		 * @Route(methods={"POST"}, name="create")
+		 *
+		 * @param Request                   $request
+		 * @param SectionRequestTransformer $transformer
+		 * @param SectionEntityTransformer  $sectionEntityTransformer
+		 *
+		 * @return Response
+		 * @throws \Exception
+		 */
+		public function create(Request $request,
+			SectionRequestTransformer $transformer,
+			SectionEntityTransformer $sectionEntityTransformer): Response {
+
+			$section = $this->doCreate($request,
+				$transformer,
+				SectionDTO::class,
+				$sectionEntityTransformer
+			);
+
+			return $this->responseHelper->createResourceCreatedResponse('section/read/' . $section->getId());
+
+		}
+
+	}
