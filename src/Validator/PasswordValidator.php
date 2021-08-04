5626abbe98bc722a406c32ebc0c19a2e04c930ee
Mason Miller <masonmiller11@gmail.com>
2021-08-03 21:31:03 -0400

    Create password constraint

--- src/Validator/PasswordValidator.php
+++ src/Validator/PasswordValidator.php
@@ -21,7 +21,7 @@
 				throw new UnexpectedTypeException($value, 'string');
 			}
 
-			if (!preg_match('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', $value, $matches)) {
+			if (!preg_match('/(?i)^(?=.*[a-z])(?=.*\d).{8,}$/', $value, $matches)) {
 				$this->context->buildViolation($constraint->message)
 					->setParameter('{{ string }}', $value)
 					->addViolation();
