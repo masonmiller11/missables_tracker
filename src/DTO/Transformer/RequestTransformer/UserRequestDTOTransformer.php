18e882f4993fa35fbc9282b74520699f72177b48
Mason Miller <masonmiller11@gmail.com>
2021-08-03 21:31:40 -0400

    Create User DTO

---
+++ src/DTO/Transformer/RequestTransformer/UserRequestDTOTransformer.php
@@ -0,0 +1,36 @@
+<?php
+	namespace App\DTO\Transformer\RequestTransformer;
+
+	use App\DTO\User\UserDTO;
+	use App\Exception\ValidationException;
+	use Symfony\Component\HttpFoundation\Request;
+
+	class UserRequestDTOTransformer extends AbstractRequestDTOTransformer {
+
+		public function transformFromRequest(Request $request): UserDTO {
+
+			$data = json_decode($request->getContent(), true);
+
+			$dto = new UserDTO();
+
+			if (!isset($data['username'])) {
+				throw new ValidationException('new users must include username');
+			}
+
+			if (!isset($data['email'])) {
+				throw new ValidationException('new users must include email');
+			}
+
+			if (!isset($data['password'])) {
+				throw new ValidationException('new users must include password');
+			}
+
+			$dto->username = $data['username'];
+			$dto->email = $data['email'];
+			$dto->password = $data['password'];
+
+			return $dto;
+
+		}
+
+	}
