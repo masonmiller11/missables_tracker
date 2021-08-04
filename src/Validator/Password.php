a94aa97973209fff718b20623ca9366b901441d3
Mason Miller <masonmiller11@gmail.com>
2021-08-03 21:31:03 -0400

    Create password constraint

--- src/Validator/Password.php
+++ src/Validator/Password.php
@@ -3,6 +3,9 @@
 
 	use Symfony\Component\Validator\Constraint;
 
+	/**
+	 * @Annotation
+	 */
 	class Password extends Constraint {
 
 		public string $message = 'Passwords must be at 8 characters in length and contain at least 1 number';
