ae1f7c68307a2ab4f61546f29269a955d27de299
Mason Miller <masonmiller11@gmail.com>
2021-09-02 11:16:42 -0400

    Migrate GameCoverArt

---
+++ migrations/Version20210901230234.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210901230234 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE game_cover_art (id INT UNSIGNED AUTO_INCREMENT NOT NULL, game_id INT UNSIGNED NOT NULL, uri VARCHAR(128) NOT NULL, INDEX IDX_9C03023FE48FD905 (game_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE game_cover_art ADD CONSTRAINT FK_9C03023FE48FD905 FOREIGN KEY (game_id) REFERENCES games (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE game_cover_art');
+    }
+}
