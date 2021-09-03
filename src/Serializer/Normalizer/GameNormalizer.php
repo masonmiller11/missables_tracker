7bb5eca66e20fdb5705f92e4ee95ee2f7b72782c
Mason Miller <masonmiller11@gmail.com>
2021-09-02 20:04:30 -0400

    Add game cover URI in GameNormalizer

--- src/Serializer/Normalizer/GameNormalizer.php
+++ src/Serializer/Normalizer/GameNormalizer.php
@@ -2,20 +2,44 @@
 	namespace App\Serializer\Normalizer;
 
 	use App\Entity\Game;
+	use App\Entity\GameCoverArt;
 	use App\Entity\Playthrough\PlaythroughTemplate;
+	use App\Service\IGDBHelper;
+	use Symfony\Component\HttpClient\Exception\ClientException;
 	use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+	use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+	use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+	use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+	use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+	use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 	class GameNormalizer implements  ContextAwareNormalizerInterface {
+
+		private IGDBHelper $IGDBHelper;
+
+		public function __construct(IGDBHelper $IGDBHelper) {
+			$this->IGDBHelper = $IGDBHelper;
+		}
 
 		/**
 		 * @param Game $object
 		 * @param string|null $format
 		 * @param array $context
 		 * @return array
+		 * @throws ClientExceptionInterface
+		 * @throws DecodingExceptionInterface
+		 * @throws RedirectionExceptionInterface
+		 * @throws ServerExceptionInterface
+		 * @throws TransportExceptionInterface
 		 */
 		public function normalize ($object, string $format = null, array $context = []): array {
 
 			$data['title'] = $object->getTitle();
+			try {
+				$data['cover'] = $this->IGDBHelper->getCoverArtWorkURIFromIGDB($object->getCover());
+			} catch (ClientException $exception) {
+				$data['cover'] = 'cover unavailable';
+			}
 			$data['templateCount'] = $object->getPlaythroughTemplateCount();
 			$data['playthroughCount'] = $object->getPlaythroughCount();
 			$data['playthroughTemplate'] = $object->getTemplates()->map(
@@ -32,7 +56,6 @@
 			$data['rating'] = $object->getRating();
 			$data['slug'] = $object->getSlug();
 			$data['screenshots'] = $object->getScreenshots();
-			$data['cover'] = $object->getCover();
 			$data['artworks'] = $object->getArtworks();
 			$data['internetGameDatabaseId'] = $object->getInternetGameDatabaseID();
 			$data['releaseDate'] = $object->getReleaseDate()->format('Y-m-d');
