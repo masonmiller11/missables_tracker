08c02c8a0ef1cf1df82ef8108b1930e70af87e94
Mason Miller <masonmiller11@gmail.com>
2021-09-19 15:23:01 -0400

    Create new doCreate method in AbstractApiController

--- src/Transformer/EntityTransformerInterface.php
+++ src/Transformer/EntityTransformerInterface.php
@@ -12,7 +12,7 @@
 	 */
 	interface EntityTransformerInterface {
 
-		public function create(DTOInterface $dto, User $user = null): EntityInterface;
+		public function create(Object $dto, User $user = null): EntityInterface;
 
 		public function update(int $id, Request $request, bool $skipValidation = false): EntityInterface;
 
