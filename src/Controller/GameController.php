0dee1f218eba6b07997c6e2c8a5086b4a0ed6f7f
Mason Miller <masonmiller11@gmail.com>
2022-02-03 00:42:33 -0500

    Only call igdb if we do not have games saved

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -14,6 +14,7 @@
 	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Component\Serializer\SerializerInterface;
 	use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
@@ -39,6 +40,7 @@
 		 * PayloadDecoderRegistryInterface's alias is set to PayloadDecoderRegistry in payload-decoder.yaml
 		 *
 		 * GameController constructor.
+		 *
 		 * @param IGDBHelper $IGDBHelper
 		 * @param GameEntityTransformer $entityTransformer
 		 * @param GameRepository $repository
@@ -101,11 +103,14 @@
 		}
 
 		/**
-		 * @Route(path="search/{searchTerm}", methods={"GET"}, name="search_game")
+		 * @Route(path="search/{searchTerm}/{page<\d+>?1}/{pageSize<\d+>?9}", methods={"GET"}, name="search_game")
 		 *
+		 * @param int $page
+		 * @param int $pageSize
 		 * @param string $searchTerm
 		 * @param SerializerInterface $serializer
 		 * @param Request $request
+		 *
 		 * @return Response
 		 *
 		 * Searches our database for title that includes the combination of letters in query.
@@ -117,41 +122,67 @@
 		 * @throws TransportExceptionInterface
 		 * @throws \Exception
 		 */
-		public function search(string $searchTerm, SerializerInterface $serializer, Request $request): Response {
+		public function search(
+			int $page, int $pageSize, string $searchTerm, SerializerInterface $serializer, Request $request
+		): Response {
 
 			try {
 
 				if (!$this->repository instanceof GameRepository)
 					throw new InvalidRepositoryException(GameRepository::class, $this->repository::class);
 
-				//Get games that we currently have saved to the database.
-				$games = $this->repository->searchByName($searchTerm);
+				try {
+					//Get games that we currently have saved to the database.
+					$games = $this->repository->searchByName($searchTerm, $page, $pageSize);
 
-				//Returns an array of IGDB data transfer objects based on the search term.
-				$igdbDtos = $this->IGDBHelper->searchIGDB($searchTerm, 9);
+					if ($games['totalItems'] > 8) {
 
-				if (!$this->entityTransformer instanceof GameEntityTransformer)
-					throw new InvalidArgumentException(
-						'Expected GameEntityTransformer. Current transformer: ' . $this->entityTransformer::class
-					);
+						return ResponseHelper::createReadResponse($games, $serializer);
 
-				//Creates games found on IGDB (if they're currently not added) and then returns the Game entities.
-				$igdbGames = $this->entityTransformer->assembleAndSaveMany($igdbDtos);
+					} else {
 
-				//Merge games that were just created (from searching IGDB) with the games that were already in database.
-				$allGames = array_merge($games, $igdbGames);
+						//Returns an array of IGDB data transfer objects based on the search term.
+						$igdbDtos = $this->IGDBHelper->searchIGDB($searchTerm, $pageSize);
+
+						if (!$this->entityTransformer instanceof GameEntityTransformer)
+							throw new InvalidArgumentException(
+								'Expected GameEntityTransformer. Current transformer: ' . $this->entityTransformer::class
+							);
+
+						//Creates games found on IGDB (if they're currently not added) and then returns the Game entities.
+						$igdbGames = $this->entityTransformer->assembleAndSaveMany($igdbDtos);
+
+						//Merge games that were just created (from searching IGDB) with the games that were already in database.
+						$allGames['items'] = array_merge($games['items'], $igdbGames);
+						$allGames['totalItems'] = $pageSize;
+
+						return ResponseHelper::createReadResponse($allGames, $serializer);
+
+					}
+
+				} catch (NotFoundHttpException) {
+
+					//Returns an array of IGDB data transfer objects based on the search term.
+					$igdbDtos = $this->IGDBHelper->searchIGDB($searchTerm, $pageSize);
+
+					if (!$this->entityTransformer instanceof GameEntityTransformer)
+						throw new InvalidArgumentException(
+							'Expected GameEntityTransformer. Current transformer: ' . $this->entityTransformer::class
+						);
+
+					//Creates games found on IGDB (if they're currently not added) and then returns the Game entities.
+					$igdbGames['items'] = $this->entityTransformer->assembleAndSaveMany($igdbDtos);
+					$igdbGames['totalItems'] = $pageSize;
+
+					return ResponseHelper::createReadResponse($igdbGames, $serializer);
+
+				}
 
 			} catch (PayloadDecoderException | ValidationException $exception) {
 
 				return $this->handleApiException($request, $exception);
 
 			}
-
-			if ($allGames == [])
-				return ResponseHelper::createJsonErrorResponse('Your search did not return any games.','error');
-
-			return ResponseHelper::createReadResponse($allGames, $serializer);
-
 
 		}
 
@@ -176,9 +207,12 @@
 			$games = $this->IGDBHelper->searchIGDB($game);
 
 			if (!$games || $games === []) {
-				return new JsonResponse(['status' => 'error',
-					'message' => $game . 'search returned no titles'
-				], Response::HTTP_NOT_FOUND);
+				return new JsonResponse(
+					[
+						'status' => 'error',
+						'message' => $game . 'search returned no titles',
+					], Response::HTTP_NOT_FOUND
+				);
 			}
 
 			return new JsonResponse($games);
